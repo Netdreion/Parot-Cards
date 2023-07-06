9224621ec8ee94d423ab748feab0af229a349f4b
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-07-06 14:37:45 -0400

    removed the useEffect and modify the shuffleCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,47 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Deck from "./components/Deck";
 import Card from "./components/Card";
 import CardsData from "./CardsData";
 
-function App() {
+const App = () => {
   const [data, setData] = useState(CardsData);
   const [cards, setCards] = useState([]);
 
-  useEffect(() => {
-    shuffleData();
-  }, []);
-
-  function shuffleData() {
+  const shuffleData = (data) => {
     const shuffledData = [...data];
     for (let i = shuffledData.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [shuffledData[i], shuffledData[j]] = [shuffledData[j], shuffledData[i]];
     }
-    setData(shuffledData);
-  }
+    return shuffledData;
+  };
 
-  function handleCardClick(card) {
-    if (cards.some((c) => c.id === card.id)) {
-      return;
-    }
-    if (cards.length > 2) {
+  const handleCardClick = (card) => {
+    if (cards.some((c) => c.id === card.id) || cards.length > 2) {
       return;
     }
 
     const newCards = [...cards, card];
-
     setCards(newCards);
-  }
+  };
 
-  function newStart() {
-    const resetData = [];
-    setCards(resetData);
-    shuffleData();
-  }
+  const newStart = () => {
+    setCards([]);
+    setData(shuffleData(data));
+  };
+
+  const shuffledData = shuffleData(data);
 
   return (
     <div>
-      <Deck handleCardClick={handleCardClick} data={data} />
+      <Deck handleCardClick={handleCardClick} data={shuffledData} />
       <Card cards={cards} />
       <button onClick={newStart}>
         {cards.length === 0 ? "Click To Start" : "New Reading"}
       </button>
     </div>
   );
-}
+};
 
 export default App;
