fe675f791e558fdd9502119c4ebb30755226dd6a
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-07-06 23:17:44 -0400

    homepage set up

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -1,6 +1,6 @@
 const WelcomePage = () => {
   return (
-    <div>
+    <div className="homepage-container">
       <header>Parrot Cards</header>
       <h1>free Tarot Reading</h1>
       <h3>The most fun reading you will ever get</h3>
