1a2e6bf7b4dd98fb7062a084f4c01abf04affb7c
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-07-06 23:03:56 -0400

    router setup to the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,15 @@
 import React, { useState } from "react";
-import "./App.css";
-import Deck from "./components/Deck";
-import Card from "./components/Card";
-import CardsData from "./CardsData";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RenderPage from "./RenderPage";
+import WelcomePage from "./components/WelcomePage";
 
 const App = () => {
-  const [data, setData] = useState(CardsData);
-  const [cards, setCards] = useState([]);
-
-  const shuffleData = (data) => {
-    const shuffledData = [...data];
-    for (let i = shuffledData.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [shuffledData[i], shuffledData[j]] = [shuffledData[j], shuffledData[i]];
-    }
-    return shuffledData;
-  };
-
-  const handleCardClick = (card) => {
-    if (cards.some((c) => c.id === card.id) || cards.length > 2) {
-      return;
-    }
-
-    const newCards = [...cards, card];
-    setCards(newCards);
-  };
-
-  const newStart = () => {
-    setCards([]);
-    setData(shuffleData(data));
-  };
-
-  const shuffledData = shuffleData(data);
-
   return (
-    <div>
-      <Deck handleCardClick={handleCardClick} data={shuffledData} />
-      <Card cards={cards} />
-      <button onClick={newStart}>
-        {cards.length === 0 ? "Click To Start" : "New Reading"}
-      </button>
-    </div>
+    <Routes>
+      <Route path="/" element={<WelcomePage />}></Route>
+      <Route path="/RenderPage" element={<RenderPage />}></Route>
+    </Routes>
   );
 };
 
---
+++ src/RenderPage.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "./App.css";
+import Deck from "./components/Deck";
+import Card from "./components/Card";
+import CardsData from "./CardsData";
+
+const RenderPage = () => {
+  const [data, setData] = useState(CardsData);
+  const [cards, setCards] = useState([]);
+
+  const shuffleData = (data) => {
+    const shuffledData = [...data];
+    for (let i = shuffledData.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledData[i], shuffledData[j]] = [shuffledData[j], shuffledData[i]];
+    }
+    return shuffledData;
+  };
+
+  const handleCardClick = (card) => {
+    if (cards.some((c) => c.id === card.id) || cards.length > 2) {
+      return;
+    }
+
+    const newCards = [...cards, card];
+    setCards(newCards);
+  };
+
+  const newStart = () => {
+    setCards([]);
+    setData(shuffleData(data));
+  };
+
+  const shuffledData = shuffleData(data);
+
+  return (
+    <div>
+      <Deck handleCardClick={handleCardClick} data={shuffledData} />
+      <Card cards={cards} />
+      <button onClick={newStart}>
+        {cards.length === 0 ? "Click To Start" : "New Reading"}
+      </button>
+    </div>
+  );
+};
+
+export default RenderPage;
