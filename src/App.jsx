e663d97d123034f1243fadbabf0afb0f78124878
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-07-07 11:16:27 -0400

    WelcomePage set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import RenderPage from "./RenderPage";
 import WelcomePage from "./components/WelcomePage";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<WelcomePage />}></Route>
-      <Route path="/RenderPage" element={<RenderPage />}></Route>
-    </Routes>
+    <>
+      <WelcomePage />
+      <Routes>
+        <Route path="/RenderPage" element={<RenderPage />}></Route>
+      </Routes>
+    </>
   );
 };
 
---
+++ src/navigations/navButton1.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+import RenderPage from "../RenderPage";
+
+export const homeNavButton = () => {
+  return (
+    <button>
+      <Link to="./RendeRPage"></Link>
+    </button>
+  );
+};
